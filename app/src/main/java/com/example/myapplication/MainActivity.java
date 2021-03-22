3a9297659187ac84871557f6db20b4892098a225
KeerthikaReddy02 <keerthi.pie.2002@gmail.com>
2021-03-22 08:49:30 +0530

    TASK-2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,46 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button buttonmove;
-    Button btn;
-    EditText et1;
-    EditText et2;
-    String st1;
-    String st2;
-
+    EditText Eemail,Epassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonmove=findViewById(R.id.button4);
-        btn=findViewById(R.id.button4);
-        et1=findViewById(R.id.email);
-        et1=findViewById(R.id.password);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i=new Intent(MainActivity.this,task2.class);
-                st1=et1.getText().toString();
-                st2=et2.getText().toString();
-                i.putExtra("email",st1);
-                i.putExtra("password",st2);
-                startActivity(i);
-                finish();
-
-            }
-        });
-        buttonmove.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                moveToActivityTwo();
-            }
-        });
-
-    }
-    private void moveToActivityTwo(){
-        Intent intent=new Intent(MainActivity.this,task2.class );
-        startActivity(intent);
+       Eemail=findViewById(R.id.email);
+       Epassword=findViewById(R.id.password);
+       Button button=findViewById(R.id.button4);
+       button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String email=Eemail.getText().toString().trim();
+               String password=Epassword.getText().toString().trim();
+               Bundle bundle=new Bundle();
+               bundle.putString("email",email);
+               bundle.putString("password",password);
+               Intent intent=new Intent(MainActivity.this,task2.class);
+               intent.putExtras(bundle);
+               startActivity(intent);
+           }
+       });
     }
 }
--- app/src/main/java/com/example/myapplication/task2.java
+++ app/src/main/java/com/example/myapplication/task2.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class task2 extends AppCompatActivity {
     private Button buttonmove;
@@ -15,24 +16,22 @@ public class task2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task2);
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String email = bundle.getString("email");
+            String password = bundle.getString("password");
+            TextView email1 = findViewById(R.id.email);
+            TextView password1 = findViewById(R.id.password);
+            email1.setText(email);
+            password1.setText(password);
+        }
         buttonmove = findViewById(R.id.button4);
-        EditText username;
-        username =(EditText)findViewById(R.id.email);
-        EditText password;
-        password =(EditText)findViewById(R.id.password);
-        EditText password2;
-        password2 =(EditText)findViewById(R.id.password2);
-
         buttonmove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                moveToActivityTwo();
+                Intent intent = new Intent(task2.this, activity_signup.class);
+                startActivity(intent);
             }
         });
-    }
-
-    private void moveToActivityTwo() {
-        Intent intent = new Intent(task2.this, signup.class);
-        startActivity(intent);
     }
 }
