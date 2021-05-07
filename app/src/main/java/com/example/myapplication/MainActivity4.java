6e01c4ce6878af5d2c42771de130a4523880bb53
KeerthikaReddy02 <keerthi.pie.2002@gmail.com>
2021-05-07 10:12:53 +0530

    TASK-2

--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -1,11 +1,16 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity4 extends AppCompatActivity {
     String contact,college,branch,email;
@@ -14,6 +19,8 @@ public class MainActivity4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        BottomNavigationView bottomNav=findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
             contact = bundle.getString("contact");
@@ -36,4 +43,31 @@ public class MainActivity4 extends AppCompatActivity {
             }
         });
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
+            switch (item.getItemId())
+            {
+                case R.id.home:
+                    selectedFragment=new HomeFragment();
+                    break;
+                case R.id.open_book:
+                    selectedFragment=new OpenBookFragment();
+                    break;
+                case R.id.plus:
+                    selectedFragment=new PlusFragment();
+                    break;
+                case R.id.renew:
+                    selectedFragment=new renewFragment();
+                    break;
+                case R.id.user:
+                    selectedFragment=new UserFragment();
+                    break;
+
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frag, selectedFragment).commit();
+            return true;
+        }
+    };
 }
